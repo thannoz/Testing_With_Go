f8db98eef9f8b812fc95a144cc3c8c5aa20af2d5
thanoz <carlos-7@hotmail.de>
2022-09-20 23:36:55 +0200

    Implement tests for arrays and slices

--- Arrays_Slices/sum.go
+++ Arrays_Slices/sum.go
@@ -6,3 +6,26 @@ func Sum(nums []int) (total int) {
 	}
 	return
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNums := len(numbersToSum)
+	sums := make([]int, lengthOfNums)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
