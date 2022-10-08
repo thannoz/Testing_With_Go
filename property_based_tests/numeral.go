4fd5397b0face8b3ad32294e7e8d319d1465be26
thanoz <carlos-7@hotmail.de>
2022-10-08 22:27:37 +0200

    Finished testing convertion from Arabic to Roman functionality

--- property_based_tests/numeral.go
+++ property_based_tests/numeral.go
@@ -1,9 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"strings"
 )
+
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+var allRomanNumerals = []RomanNumeral{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
 
 func ConvertToRoman(arabic int) string {
 
@@ -11,23 +31,12 @@ func ConvertToRoman(arabic int) string {
 	// It minimizes memory copying.
 	var result strings.Builder
 
-	for arabic > 0 {
-		switch {
-		case arabic > 8:
-			result.WriteString("IX")
-			arabic -= 9
-		case arabic > 4:
-			result.WriteString("V")
-			arabic -= 5
-		case arabic > 3:
-			result.WriteString("IV")
-			arabic -= 4
-		default:
-			result.WriteString("I")
-			arabic--
+	for _, numeral := range allRomanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
 		}
 	}
 
-	fmt.Println("result:", result.String())
 	return result.String()
 }
