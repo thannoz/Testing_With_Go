59ad02515a6e4e4e861631c93c99244a681215da
thanoz <carlos-7@hotmail.de>
2022-10-14 10:52:54 +0200

    Added test for templates

--- templating/blogrenderer.go
+++ templating/blogrenderer.go
@@ -1,6 +1,8 @@
 package blogrenderer
 
 import (
+	"embed"
+	"html/template"
 	"io"
 )
 
@@ -9,10 +11,24 @@ type Post struct {
 	Tags                     []string
 }
 
-func Render(w *io.Writer, p Post) error {
-	return nil
+var (
+	//go:embed "templates/*"
+	postTemplates embed.FS
+)
+
+func Render(w io.Writer, p Post) error {
+	tmpl, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+	if err != nil {
+		return err
+	}
+
+	if err = tmpl.Execute(w, p); err != nil {
+		return err
+	}
+
+	return err
 }
 
-func (p *Post) blogrenderer(input io.Writer) {
+// func (p *Post) blogrenderer(input io.Writer) {
 
-}
+// }
--- templating/blogrenderer_test.go
+++ templating/blogrenderer_test.go
@@ -1,9 +1,11 @@
 package blogrenderer_test
 
 import (
-	"blogrenderer"
+	blogrenderer "blogrenderer"
 	"bytes"
 	"testing"
+
+	approvals "github.com/approvals/go-approval-tests"
 )
 
 func TestRender(t *testing.T) {
@@ -17,15 +19,11 @@ func TestRender(t *testing.T) {
 	)
 	t.Run("it converts a single post into HTML", func(t *testing.T) {
 		buf := bytes.Buffer{}
-		err := blogrenderer.Render(&buf, aPost)
 
-		if err != nil {
+		if err := blogrenderer.Render(&buf, aPost); err != nil {
 			t.Fatal(err)
 		}
-		got := buf.String()
-		want := `<h1>hello world</h1>`
-		if got != want {
-			t.Errorf("got '%s' want '%s'", got, want)
-		}
+
+		approvals.VerifyString(t, buf.String())
 	})
 }
