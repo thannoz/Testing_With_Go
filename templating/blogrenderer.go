7c22f0c8d104e3e6f4ef07bb36d3082cee1126a9
thanoz <carlos-7@hotmail.de>
2022-10-14 19:56:33 +0200

    Finish writing unit tests for templates

--- templating/blogrenderer.go
+++ templating/blogrenderer.go
@@ -4,6 +4,7 @@ import (
 	"embed"
 	"html/template"
 	"io"
+	"strings"
 )
 
 type Post struct {
@@ -11,24 +12,36 @@ type Post struct {
 	Tags                     []string
 }
 
+type PostViewModel struct {
+	Title, SanitisedTitle, Description, Body string
+	Tags                                     []string
+}
+
 var (
 	//go:embed "templates/*"
 	postTemplates embed.FS
 )
 
-func Render(w io.Writer, p Post) error {
-	tmpl, err := template.ParseFS(postTemplates, "templates/*.gohtml")
-	if err != nil {
-		return err
-	}
-
-	if err = tmpl.Execute(w, p); err != nil {
-		return err
-	}
-
-	return err
+type PostRenderer struct {
+	templ *template.Template
 }
 
-// func (p *Post) blogrenderer(input io.Writer) {
+func NewPostRender() (*PostRenderer, error) {
+	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+	if err != nil {
+		return nil, err
+	}
+	return &PostRenderer{templ: templ}, nil
+}
 
-// }
+func (r *PostRenderer) Render(w io.Writer, p Post) error {
+	return r.templ.ExecuteTemplate(w, "blog.gohtml", p)
+}
+
+func (p Post) SanitisedTitle() string {
+	return strings.ToLower(strings.Replace(p.Title, " ", "_", -1))
+}
+
+func (r *PostRenderer) RenderIndex(w io.Writer, posts []Post) error {
+	return r.templ.ExecuteTemplate(w, "index.gohtml", posts)
+}
